bb829b218cd7aea9c7c0681aee724ef808826302
kochewww <vladusta2@gmail.com>
2020-02-07 20:40:11 +0300

    input added, draft styling beginned

--- src/DraftBar.js
+++ src/DraftBar.js
@@ -4,17 +4,31 @@ import styled from "styled-components";
 export default () => {
   const DraftBar = styled.div`
     display: flex;
-    justify-content: center;
-    align-items: flex-start;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
     border: 1px solid;
     min-height: 16rem;
     max-height: 100%;
     margin-top: -16rem;
   `;
+  const Header = styled.h3`
+    display: flex;
+  `;
+  const TextArea = styled.textarea`
+    dispay: flex;
+  `;
+  const TextCard = styled.div`
+    display: flex;
+    margi: 1rem;
+  `;
   return (
     <div className="col-xs-3">
       <DraftBar>
-        <h3>Draft</h3>
+        <Header>Draft</Header>
+        <TextCard>
+          <TextArea></TextArea>
+        </TextCard>
       </DraftBar>
     </div>
   );
--- src/InputBar.js
+++ src/InputBar.js
@@ -3,15 +3,37 @@ import styled from "styled-components";
 
 export default () => {
   const InputBar = styled.div`
-    display: block;
-    justify-content: center;
-    align-content: center;
+    display: flex;
+    justify-content: flex-start;
+    align-content: flex-start;
     border: 1px solid;
     height: 8rem;
+    box-sizing: border-box;
+  `;
+  const Input = styled.input`
+    display: flex;
+    margin: 1rem;
+    height: 1rem;
+  `;
+  const Button = styled.button`
+    dispay: flex;
+    background: ${props => props.color || "grey"};
+    height: 2rem;
+    width: ${props => props.length || "3rem"};
+    position: absolute;
+    margin-top: 3rem;
+    margin-left: ${props => props.marginLeft || "1rem"};
+    border: none;
   `;
   return (
     <div className="col-xs-3">
-      <InputBar></InputBar>
+      <InputBar>
+        <Input />
+        <Button>Draft</Button>
+        <Button marginLeft="5rem" color="green" length="8.5rem">
+          Save
+        </Button>
+      </InputBar>
     </div>
   );
 };
