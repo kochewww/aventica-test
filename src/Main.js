d46b1f195f347e5923cdd67551f0a53796bd0c50
kochewww <vladusta2@gmail.com>
2020-02-11 12:37:06 +0300

    container changed to prevent overlaping of blocks on small screens

--- src/Main.js
+++ src/Main.js
@@ -3,15 +3,15 @@ import styled from "styled-components";
 import SavedCards from "./ui/SavedCards";
 const Main = styled.div`
   position: absolute;
-  top: 93px;
+  top: 82px;
   display: flex;
   flex-wrap: wrap;
   justify-content: flex-start;
   align-items: flex-start;
   border: 1px solid;
   min-height: 25rem;
-  width: 63%;
-  left: 29.5%;
+  width: 72.1%;
+  left: 27%;
 `;
 export default () => {
   return (
